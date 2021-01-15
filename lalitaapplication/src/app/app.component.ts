36dc33042eb1be3b1be74f173a8134dc3a0033fb
Lalita Ossawy <lalita.ossawy@allsop.software>
2021-01-15 00:27:43 +0000

    End of Section 5

--- lalitaapplication/src/app/app.component.ts
+++ lalitaapplication/src/app/app.component.ts
@@ -1,34 +1,29 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
-@Component({ //Decorator - a way of giving a normal class extra abilities
+
+@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Lalitas Dating Application';  //Class property - interpolation on HTML '{{user | json}}' 
-
-  //Typescript gives us type safety unless using 'any'
+  title = 'The Dating app';
   users: any;
 
-  //Construct the app component then get the HttpClient
-  //Dependancy Injection
-  constructor(private http: HttpClient) { } //Asynchronous Request
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
-    this.getUsers(); //Calling the getUsers methods
+    this.setCurrentUser();
   }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => { //Using a HTTP request get service - a URL
-      this.users = response; //Response is going to contain the users from the API server
-    }, error => {
-      console.log(error);  //In the case of an error - throw the error to the console
-    })
+s
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user)
   }
 }
-
 
 /* -- NOTES
 
@@ -36,7 +31,7 @@ Use 'this' to use any property within the class
 '?' these things are optional, do not need to provide these e.g. 'header?'
 Response body means the data is available
 .subscribe will give what return data or a error if the request is invalid
-CORS - Cross-origin resource sharing 
+CORS - Cross-origin resource sharing
 
 -- */
 
--- lalitaapplication/src/app/app.module.ts
+++ lalitaapplication/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+// import { HomeComponent } from './home/home.component';
+import { HomeComponent } from '../app/home/home.component';
 import { NgModule } from '@angular/core';
-
+import { RegisterComponent } from '../app/pages/register/register.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({ //This in an angular module
-  declarations: [ //components available in our application - can import more
-    AppComponent
-  ],
+  declarations: [	 //components available in our application - can import more
+    AppComponent, 
+    NavigationComponent,
+      HomeComponent,
+      RegisterComponent
+   ],
   imports: [
     BrowserModule, //Single page application - so our code can be displayed in our browser
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent] //Components added when our application is bootstrapped
